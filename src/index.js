6c3fc8b1fa79dba9959b3d097f599968da22ff70
jazz287 <wadhwa.jasmine28@gmail.com>
2024-06-24 00:51:52 +0530

    added cors middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import bodyParser from "body-parser";
 
 const app = express();
 
+// CORS Middleware
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
 // parse json
 app.use(bodyParser.json());
 
